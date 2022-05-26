2102a241ba1e5cad12c39a231cbad6d1f6a256e3
daniel <daniel217032001@gmail.com>
2022-05-26 23:17:11 +0300

    update

--- includes/class-wc-reepay-renewals.php
+++ includes/class-wc-reepay-renewals.php
@@ -6,19 +6,24 @@
  * @since 1.0.0
  */
 class WC_Reepay_Renewals {
+	/**
+	 * @var WC_Reepay_Api
+	 */
+	public $api;
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		add_action( 'woocommerce_payment_complete', [ $this, 'add_renewal' ] );
+		add_action( 'reepay_payment_finalize ', [ $this, 'add_renewal' ] );
 	}
 
-	public function add_renewal( $order_id ) {
-		if ( ! WC_Reepay_Helpers::is_order_paid_via_reepay( $order_id ) ) {
-			return;
-		}
-
+	/**
+	 * @param array<string, mixed> $result
+	 *
+	 * @see https://reference.reepay.com/api/#get-invoice
+	 */
+	public function add_renewal( $result ) {
 
 	}
 }
