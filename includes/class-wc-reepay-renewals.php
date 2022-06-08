2938bff8081c0d51a5d9d1e1dfac90182c5e1122
daniel <daniel217032001@gmail.com>
2022-06-09 02:16:08 +0300

    update

--- includes/class-wc-reepay-renewals.php
+++ includes/class-wc-reepay-renewals.php
@@ -27,28 +27,45 @@ class WC_Reepay_Renewals {
 	 * ] $data
 	 */
 	public function add_renewal( $data ) {
-		$res = null;
+		$order = wc_get_order( $data['order_id'] );
 
-		try {
-			/**
-			 * @see https://reference.reepay.com/api/#create-subscription
-			 */
-			$res = reepay_s()->api()->request( 'subscription', 'POST', [
-				'customer'       => $data['customer'],
-				'plan'           => $data['plan'],
+		if ( empty( $order ) || ! WC_Reepay_Helpers::is_order_paid_via_reepay( $order ) ) {
+			return;
+		}
+
+		$token = self::get_payment_token_order( $order );
+
+		if ( empty( $token ) ) {
+			return;
+		}
+
+		$token = $token->get_token();
+
+		foreach ( $order->get_items() as $item_id => $item ) {
+			$product = $item->get_product();
+			$handle = 'subscription_handle_' . $order->get_id() . '_' . $product->get_id();
+			$res = null;
+
+			try {
+				/**
+				 * @see https://reference.reepay.com/api/#create-subscription
+				 */
+				$res = reepay_s()->api()->request( 'subscription', 'POST', [
+					'customer'       => $data['customer'],
+					'plan'           => $product->get_meta( '_reepay_subscription_handle' ),
 //					'amount' => null,
-//					'quantity' => null,
+					'quantity'       => $item->get_quantity(),
 //					'test' => null,
-//					'handle' => null,
+					'handle'         => $handle,
 //					'metadata' => null,
-//					'source' => null,
+					'source'         => $token,
 //					'create_customer' => null,
 //					'plan_version' => null,
 //					'amount_incl_vat' => null,
 //					'generate_handle' => null,
 //					'start_date' => null,
 //					'end_date' => null,
-				'grace_duration' => 172800,
+					'grace_duration' => 172800,
 //					'no_trial' => null,
 //					'no_setup_fee' => null,
 //					'trial_period' => null,
@@ -56,26 +73,61 @@ class WC_Reepay_Renewals {
 //					'coupon_codes' => null,
 //					'add_ons' => null,
 //					'additional_costs' => null,
-				'signup_method'  => 'source',
-			] );
-		} catch ( Exception $e ) {
-		}
+					'signup_method'  => 'source',
+				] );
+			} catch ( Exception $e ) {
+			}
 
-		if ( ! empty( $res ) ) {
+			if ( empty( $res ) ) {
+				return;
+			}
+
 			try {
 				/**
 				 * @see https://reference.reepay.com/api/#set-payment-method
 				 */
 				$res = reepay_s()->api()->request( "subscription/{$res['handle']}/pm", 'POST', [
 					'handle' => $res['handle'],
-//						'source'          => null,
-//						'payment_method_reference' => null,
+					'source' => $token,
 				] );
 			} catch ( Exception $e ) {
 			}
 		}
+	}
 
-		die();
+	/**
+	 * Get payment token.
+	 *
+	 * @param  WC_Order  $order
+	 *
+	 * @return WC_Payment_Token_Reepay|false
+	 */
+	public static function get_payment_token_order( WC_Order $order ) {
+		$token = $order->get_meta( '_reepay_token' );
+		if ( empty( $token ) ) {
+			return false;
+		}
+
+		return self::get_payment_token( $token );
+	}
+
+	/**
+	 * Get Payment Token by Token string.
+	 *
+	 * @param  string  $token
+	 *
+	 * @return null|bool|WC_Payment_Token
+	 */
+	public static function get_payment_token( $token ) {
+		global $wpdb;
+
+		$query    = "SELECT token_id FROM {$wpdb->prefix}woocommerce_payment_tokens WHERE token = '%s';";
+		$token_id = $wpdb->get_var( $wpdb->prepare( $query, $token ) );
+		if ( ! $token_id ) {
+			return false;
+		}
+
+		return WC_Payment_Tokens::get( $token_id );
 	}
 }
 
