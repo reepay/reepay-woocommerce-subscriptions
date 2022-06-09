381f659814cfe86483631c9a2ff2e373160e7c46
daniel <daniel217032001@gmail.com>
2022-06-09 18:26:46 +0300

    update WC_Reepay_Renewals::create_subscription

--- includes/class-wc-reepay-renewals.php
+++ includes/class-wc-reepay-renewals.php
@@ -10,7 +10,7 @@ class WC_Reepay_Renewals {
 	 * Constructor
 	 */
 	public function __construct() {
-		add_action( 'reepay_webhook_invoice_authorized', [ $this, 'add_renewal' ] );
+		add_action( 'reepay_webhook_invoice_authorized', [ $this, 'create_subscription' ] );
 	}
 
 	/**
@@ -26,7 +26,7 @@ class WC_Reepay_Renewals {
 	 *     'order_id' => int
 	 * ] $data
 	 */
-	public function add_renewal( $data ) {
+	public function create_subscription( $data ) {
 		$order = wc_get_order( $data['order_id'] );
 
 		if ( empty( $order ) || ! WC_Reepay_Helpers::is_order_paid_via_reepay( $order ) ) {
@@ -36,36 +36,42 @@ class WC_Reepay_Renewals {
 		$token = self::get_payment_token_order( $order );
 
 		if ( empty( $token ) ) {
+			reepay_s()->log()->log([
+				'source' => 'WC_Reepay_Renewals::create_subscription',
+				'error' => 'Empty token',
+				'data' => $data
+			], 'error');
 			return;
 		}
 
 		$token = $token->get_token();
 
-		foreach ( $order->get_items() as $item_id => $item ) {
-			$product = $item->get_product();
-			$handle = 'subscription_handle_' . $order->get_id() . '_' . $product->get_id();
-			$res = null;
+		$order_item = $order->get_items()[0];
+		$product = $order_item->get_product();
 
-			try {
-				/**
-				 * @see https://reference.reepay.com/api/#create-subscription
-				 */
-				$res = reepay_s()->api()->request( 'subscription', 'POST', [
-					'customer'       => $data['customer'],
-					'plan'           => $product->get_meta( '_reepay_subscription_handle' ),
+		$handle  = 'subscription_handle_' . $order->get_id() . '_' . $product->get_id();
+
+		$new_subscription     = null;
+		try {
+			/**
+			 * @see https://reference.reepay.com/api/#create-subscription
+			 */
+			$new_subscription = reepay_s()->api()->request( 'subscription', 'POST', [
+				'customer'       => $data['customer'],
+				'plan'           => $product->get_meta( '_reepay_subscription_handle' ),
 //					'amount' => null,
-					'quantity'       => $item->get_quantity(),
+				'quantity'       => $order_item->get_quantity(),
 //					'test' => null,
-					'handle'         => $handle,
+				'handle'         => $handle,
 //					'metadata' => null,
-					'source'         => $token,
+				'source'         => $token,
 //					'create_customer' => null,
 //					'plan_version' => null,
 //					'amount_incl_vat' => null,
 //					'generate_handle' => null,
 //					'start_date' => null,
 //					'end_date' => null,
-					'grace_duration' => 172800,
+				'grace_duration' => 172800,
 //					'no_trial' => null,
 //					'no_setup_fee' => null,
 //					'trial_period' => null,
@@ -73,26 +79,41 @@ class WC_Reepay_Renewals {
 //					'coupon_codes' => null,
 //					'add_ons' => null,
 //					'additional_costs' => null,
-					'signup_method'  => 'source',
-				] );
-			} catch ( Exception $e ) {
-			}
-
-			if ( empty( $res ) ) {
-				return;
-			}
-
-			try {
-				/**
-				 * @see https://reference.reepay.com/api/#set-payment-method
-				 */
-				$res = reepay_s()->api()->request( "subscription/{$res['handle']}/pm", 'POST', [
-					'handle' => $res['handle'],
-					'source' => $token,
-				] );
-			} catch ( Exception $e ) {
-			}
+				'signup_method'  => 'source',
+			] );
+		} catch ( Exception $e ) {
 		}
+
+		if ( empty( $new_subscription ) ) {
+			reepay_s()->log()->log([
+				'source' => 'WC_Reepay_Renewals::create_subscription',
+				'error' => 'create-subscription',
+				'data' => $data
+			], 'error');
+			return;
+		}
+
+		try {
+			/**
+			 * @see https://reference.reepay.com/api/#set-payment-method
+			 */
+			$payment_method = reepay_s()->api()->request( "subscription/{$new_subscription['handle']}/pm", 'POST', [
+				'handle' => $new_subscription['handle'],
+				'source' => $token,
+			] );
+		} catch ( Exception $e ) {
+		}
+
+		if ( empty( $payment_method ) ) {
+			reepay_s()->log()->log([
+				'source' => 'WC_Reepay_Renewals::create_subscription',
+				'error' => 'set-payment-method',
+				'data' => $data
+			], 'error');
+			return;
+		}
+
+		$order->add_meta_data( 'subscription_handle', $payment_method['handle'] );
 	}
 
 	/**
