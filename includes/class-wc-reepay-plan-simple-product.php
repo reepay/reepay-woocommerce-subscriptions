ada285379fbbea26550bfb4d8f9b8934325c30ee
daniel <daniel217032001@gmail.com>
2022-06-27 17:55:59 +0300

    update WC_Product_Reepay_Variable_Subscription::get_price_html

--- includes/class-wc-reepay-plan-simple-product.php
+++ includes/class-wc-reepay-plan-simple-product.php
@@ -58,14 +58,16 @@ class WC_Product_Reepay_Simple_Subscription extends WC_Product_Simple {
 	 *
 	 * @return string
 	 */
-    public function get_price_html( $deprecated = '' ) {
-	    $schedule_type = $this->get_meta('_reepay_subscription_schedule_type');
-	    $schedule_type = WC_Reepay_Subscription_Plan_Simple::$schedule_types[$schedule_type] ?? '';
+	public function get_price_html( $deprecated = '' ) {
+		$schedule_type = $this->get_meta( '_reepay_subscription_schedule_type' );
+		$schedule_type = WC_Reepay_Subscription_Plan_Simple::$schedule_types[ $schedule_type ]??'';
 
-	    if(empty($schedule_type)) {
-		    return parent::get_price_html();
-	    }
+		$html = parent::get_price_html();
 
-	    return parent::get_price_html() . ' / ' . $schedule_type;
-    }
+		if ( empty( $schedule_type ) || empty( $html ) ) {
+			return $html;
+		}
+
+		return parent::get_price_html()  . ' / ' . $schedule_type;
+	}
 }
