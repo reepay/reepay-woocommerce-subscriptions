dbea376adff5cc414945b821a37c3eeaa3b7eb68
daniel <daniel217032001@gmail.com>
2022-07-07 04:07:18 +0300

    add WC_Reepay_Subscription_Addons_Rest

---
+++ includes/class-wc-reepay-addons-rest.php
@@ -0,0 +1,36 @@
+<?php
+
+class WC_Reepay_Subscription_Addons_Rest extends WC_Reepay_Subscription_Plan_Simple_Rest {
+	public function init() {
+		$this->namespace = reepay_s()->settings( 'rest_api_namespace' );
+		$this->rest_base = "/addon/";
+	}
+
+	/**
+	 * Retrieves one item from the collection.
+	 *
+	 * @param  WP_REST_Request  $request  Full details about the request.
+	 *
+	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
+	 * @throws Exception
+	 * @since 4.7.0
+	 *
+	 */
+	public function get_item( $request ) {
+		try {
+			return new WP_REST_Response(
+				reepay_s()->api()->request( "add_on/{$request['handle']}" )
+			);
+		}catch( Exception $e ) {
+			reepay_s()->log()->log( [
+				'source'  => 'WC_Reepay_Subscription_Addons_Rest::get_item',
+				'message' => 'Getting add_on error',
+				'handle'  => $request['handle']
+			], 'error' );
+
+			return new WP_Error( 400, $e->getMessage() );
+		}
+	}
+}
+
+new WC_Reepay_Subscription_Addons_Rest();
--- reepay-woocommerce-subscriptions.php
+++ reepay-woocommerce-subscriptions.php
@@ -276,9 +276,10 @@ class WooCommerce_Reepay_Subscriptions{
 	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-checkout.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-simple.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-variable.php' );
+        include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-addons.php' );
 	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-simple-rest.php' );
 	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-variable-rest.php' );
-        include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-addons.php' );
+	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-addons-rest.php' );
 	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-renewals.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-discounts-and-coupons.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-account-page.php' );
