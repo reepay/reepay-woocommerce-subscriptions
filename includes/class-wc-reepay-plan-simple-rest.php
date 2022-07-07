d15bbfb54388250a19be4815dc997609dbdb968d
daniel <daniel217032001@gmail.com>
2022-07-07 03:42:39 +0300

    add WC_Reepay_Subscription_Plan_Simple_Rest

---
+++ includes/class-wc-reepay-plan-simple-rest.php
@@ -0,0 +1,75 @@
+<?php
+
+class WC_Reepay_Subscription_Plan_Simple_Rest extends WP_REST_Controller {
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+	}
+
+	public function get_path() {
+		return get_rest_url( 0, $this->namespace . $this->rest_base );
+	}
+
+	public function init() {
+		$this->namespace = reepay_s()->settings( 'rest_api_namespace' );
+		$this->rest_base = "/plan_simple/";
+	}
+
+	public function register_routes() {
+		register_rest_route( $this->namespace, $this->rest_base, [
+			"methods"             => WP_REST_Server::READABLE,
+			"callback"            => array( $this, "get_item" ),
+			"permission_callback" => array( $this, "get_item_permissions_check" ),
+			"args"                => array(
+				"handle" => array(
+					"type"              => "string",
+					"required"          => true,
+					"validate_callback" => function( $param, $request, $key ) {
+						return ! empty( $param );
+					},
+				),
+			)
+		] );
+	}
+
+	/**
+	 * Retrieves one item from the collection.
+	 *
+	 * @param  WP_REST_Request  $request  Full details about the request.
+	 *
+	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
+	 * @throws Exception
+	 * @since 4.7.0
+	 *
+	 */
+	public function get_item( $request ) {
+		try {
+			wp_send_json_success(
+				reepay_s()->api()->request("plan/{$request['handle']}")
+			);
+		} catch(Exception $e) {
+			reepay_s()->log()->log( [
+				'source' => 'WC_Reepay_Subscription_Plan_Simple_Rest::get_item',
+				'message' => 'Getting plan error',
+				'handle' => $request['handle']
+			], 'error' );
+
+			wp_send_json_error();
+		}
+	}
+
+	/**
+	 * Checks if a given request has access to get a specific item.
+	 *
+	 * @param  WP_REST_Request  $request  Full details about the request.
+	 *
+	 * @return true|WP_Error True if the request has read access for the item, WP_Error object otherwise.
+	 * @since 4.7.0
+	 *
+	 */
+	public function get_item_permissions_check( $request ) {
+		return true;
+	}
+}
+
+new WC_Reepay_Subscription_Plan_Simple_Rest();
--- reepay-woocommerce-subscriptions.php
+++ reepay-woocommerce-subscriptions.php
@@ -39,10 +39,10 @@ class WooCommerce_Reepay_Subscriptions{
 	 */
 	private static $settings;
 
-    /**
-     * @var string
-     */
-    public static $version = '1.0.0';
+	/**
+	 * @var string
+	 */
+	public static $version = '1.0.0';
 
 	/**
 	 * @var string
@@ -276,6 +276,7 @@ class WooCommerce_Reepay_Subscriptions{
 	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-checkout.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-simple.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-variable.php' );
+	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-plan-simple-rest.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-addons.php' );
 	    include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-renewals.php' );
         include_once( $this->settings('plugin_path') . '/includes/class-wc-reepay-discounts-and-coupons.php' );
