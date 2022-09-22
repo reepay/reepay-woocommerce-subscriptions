3db96dfc5412128e7d14ccf44e69ee349a0e9063
daniel21703 <daniel217032001@gmail.com>
2022-09-22 19:31:39 +0300

    update composer and psr-4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'reepay/reepay-subscriptions-for-woocommerce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '295d6c3acc3e39025ec32b5cf497a6ba62ee94d9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'reepay/reepay-subscriptions-for-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '295d6c3acc3e39025ec32b5cf497a6ba62ee94d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
