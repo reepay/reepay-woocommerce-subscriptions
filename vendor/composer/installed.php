808ad42b94b7df32fef241a3b7bb4e7d1386c2de
dimaspolohov@yandex.ru <Zenitkms123>
2022-09-22 20:36:48 +0300

    Autoload Fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'reepay/reepay-subscriptions-for-woocommerce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b3b06a9fe3bdad249da4b32baa737df66ffd0661',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'reepay/reepay-subscriptions-for-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b3b06a9fe3bdad249da4b32baa737df66ffd0661',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
