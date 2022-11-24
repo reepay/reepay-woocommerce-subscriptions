bfab8d9932a2d71b36f58cf9f4d6e1f0b907051b
daniel21703 <daniel217032001@gmail.com>
2022-11-24 19:25:02 +0300

    update WC_Reepay_Sync_Subscriptions

--- includes/sync/WC_Reepay_Sync_Subscriptions.php
+++ includes/sync/WC_Reepay_Sync_Subscriptions.php
@@ -8,6 +8,7 @@
 class WC_Reepay_Sync_Subscriptions {
 	public static $events = array(
 		'created',
+		'payment_method_added',
 	);
 
 	/**
@@ -31,23 +32,39 @@ class WC_Reepay_Sync_Subscriptions {
 	 *     ] $data
 	 */
 	public function created( $data ) {
-		if ( ! WC_Reepay_Import_Helpers::woo_reepay_subscription_exists( $data['subscription'] ) ) {
-			try {
-				/**
-				 * @see https://reference.reepay.com/api/#get-subscription
-				 **/
-				$subscription_data = reepay_s()->api()->request( "subscription/{$data['subscription']}" );
-			} catch ( Exception $e ) {
-				reepay_s()->log()->log( [
-					'source'   => 'WC_Reepay_Sync_Subscriptions::created',
-					'message'  => $e->getMessage(),
-					'$data'  => $data,
-				] );
-
-				return;
-			}
-
-			WC_Reepay_Import_Helpers::import_reepay_subscription( $subscription_data );
+		if ( WC_Reepay_Import_Helpers::woo_reepay_subscription_exists( $data['subscription'] ) ) {
+			return;
 		}
+		try {
+			/**
+			 * @see https://reference.reepay.com/api/#get-subscription
+			 **/
+			$subscription_data = reepay_s()->api()->request( "subscription/{$data['subscription']}" );
+		} catch ( Exception $e ) {
+			reepay_s()->log()->log( [
+				'source'  => 'WC_Reepay_Sync_Subscriptions::created',
+				'message' => $e->getMessage(),
+				'$data'   => $data,
+			] );
+
+			return;
+		}
+
+		WC_Reepay_Import_Helpers::import_reepay_subscription( $subscription_data );
+	}
+
+	/**
+	 * @param  array[
+	 *     'id' => string
+	 *     'timestamp' => string
+	 *     'signature' => string
+	 *     'subscription' => string
+	 *     'customer' => string
+	 *     'event_type' => string
+	 *     'event_id' => string
+	 *     ] $data
+	 */
+	public function payment_method_added( $data ) {
+
 	}
 }
