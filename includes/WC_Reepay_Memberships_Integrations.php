adabc8aa30c925e165848ab91b1f3f3ac9250b6d
dimaspolohov@yandex.ru <Zenitkms123>
2023-01-09 13:18:36 +0300

    Activation feature

--- includes/WC_Reepay_Memberships_Integrations.php
+++ includes/WC_Reepay_Memberships_Integrations.php
@@ -8,6 +8,13 @@
 class WC_Reepay_Memberships_Integrations {
 	public function __construct() {
 		add_filter( 'woocommerce_is_subscription', [ $this, 'add_reepay_subscriptions_type' ], 100, 3 );
+
+		if ( ! is_plugin_active( 'woocommerce-subscriptions/woocommerce-subscriptions.php' ) ) {
+			$plugins   = get_option( 'active_plugins', array() );
+			$plugins[] = 'woocommerce-subscriptions/woocommerce-subscriptions.php';
+			update_option( 'active_plugins', $plugins );
+		}
+
 	}
 
 	/**
@@ -15,7 +22,13 @@ class WC_Reepay_Memberships_Integrations {
 	 * @param int $product_id
 	 * @param WC_Product $product
 	 */
-	public function add_reepay_subscriptions_type($is_subscription, $product_id, $product) {
+	public function add_reepay_subscriptions_type( $is_subscription, $product_id, $product ) {
 		return $is_subscription || WC_Reepay_Checkout::is_reepay_product( $product );
+	}
+}
+
+if ( ! class_exists( 'WC_Subscription' ) ) {
+	class WC_Subscription {
+
 	}
 }
