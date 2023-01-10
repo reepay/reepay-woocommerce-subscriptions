d785ada845b8a89187a5d3461487990430ca56c5
daniel21703 <daniel217032001@gmail.com>
2023-01-10 12:32:46 +0300

    update fake woo subs activation

--- includes/WC_Reepay_Memberships_Integrations.php
+++ includes/WC_Reepay_Memberships_Integrations.php
@@ -6,17 +6,34 @@
  * @since 1.0.9
  */
 class WC_Reepay_Memberships_Integrations {
-	public function __construct() {
-		add_filter( 'woocommerce_is_subscription', [ $this, 'add_reepay_subscriptions_type' ], 100, 3 );
+	/**
+	 * true if "woo memberships" is active but "woo subscriptions" is disabled
+	 *
+	 * @var bool
+	 */
+	public static $woo_subscriptions_fake_activation = false;
 
+	public function __construct() {
+		if ( is_plugin_active( 'woocommerce-memberships/woocommerce-memberships.php' ) ) {
+			$this->fake_woo_subscription_activation();
+			
+			add_filter( 'woocommerce_is_subscription', [ $this, 'add_reepay_subscriptions_type' ], 100, 3 );
+//		    add_filter( 'wc_memberships_plan_grants_access_while_subscription_active', [ $this, 'grant_access_to_memberships' ], 100, 3 );
+			add_filter( 'wc_memberships_access_granting_purchased_product_id', [ $this, 'access_granting_purchased_product_id' ] );
+		}
+	}
+
+	/**
+	 * Add "woocommerce-subscriptions" to list of active plugins
+	 */
+	public function fake_woo_subscription_activation() {
 		if ( ! is_plugin_active( 'woocommerce-subscriptions/woocommerce-subscriptions.php' ) ) {
 			$plugins   = get_option( 'active_plugins', array() );
 			$plugins[] = 'woocommerce-subscriptions/woocommerce-subscriptions.php';
 			update_option( 'active_plugins', $plugins );
-		}
 
-//		add_filter( 'wc_memberships_plan_grants_access_while_subscription_active', [ $this, 'grant_access_to_memberships' ], 100, 3 );
-		add_filter( 'wc_memberships_access_granting_purchased_product_id', [$this, 'access_granting_purchased_product_id']);
+			self::$woo_subscriptions_fake_activation = true;
+		}
 	}
 
 	/**
@@ -41,7 +58,7 @@ class WC_Reepay_Memberships_Integrations {
 	 * @param int|int[] $product_ids Product id or array of product ids that grant access (may be non unique)
 	 * @param int[] $access_granting_product_ids Array of product ids that can grant access to this plan
 	 * @param \WC_Memberships_Membership_Plan $plan Membership plan access will be granted to
-	*/
+	 */
 	public function access_granting_purchased_product_id( $product_ids, $access_granting_product_ids, $plan ) {
 		if ( ! is_array( $product_ids ) ) {
 			$product_ids = [ $product_ids ];
@@ -96,7 +113,8 @@ class WC_Reepay_Memberships_Integrations {
 	}
 }
 
-if ( ! class_exists( 'WC_Subscription' ) ) {
+if ( is_plugin_active( 'woocommerce-memberships/woocommerce-memberships.php' ) &&
+     ! class_exists( 'WC_Subscription' ) ) {
 	class WC_Subscription {
 
 	}
