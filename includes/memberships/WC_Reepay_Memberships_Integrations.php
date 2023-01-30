f6b56aa7a295d2fef542a350ad3f99b71c4a9e16
daniel21703 <daniel217032001@gmail.com>
2023-01-30 13:45:54 +0300

    test code

--- includes/memberships/WC_Reepay_Memberships_Integrations.php
+++ includes/memberships/WC_Reepay_Memberships_Integrations.php
@@ -18,6 +18,8 @@ if ( ! class_exists( 'WC_Reepay_Memberships_Integrations' ) ) {
 			if ( is_plugin_active( 'woocommerce-memberships/woocommerce-memberships.php' ) ) {
 				$this->fake_woo_subscription_activation();
 
+				add_filter( 'woocommerce_is_subscription', [ $this, 'add_reepay_subscriptions_type' ], 100, 3 );
+				
 				add_action( 'reepay_subscriptions_orders_created', [ $this, 'maybe_activate_membership' ] );
 
 				add_filter( 'wc_memberships_access_granting_purchased_product_id', [ $this, 'disable_default_membership_activation_for_reepay_products' ], 100, 3 );
@@ -39,39 +41,108 @@ if ( ! class_exists( 'WC_Reepay_Memberships_Integrations' ) ) {
 		}
 
 		/**
+		 * @param  bool        $is_subscription
+		 * @param  int         $product_id
+		 * @param  WC_Product  $product
+		 */
+		public function add_reepay_subscriptions_type( $is_subscription, $product_id, $product ) {
+			if ( is_admin() ) {
+				$is_subscription = $is_subscription || WC_Reepay_Checkout::is_reepay_product( $product );
+			}
+
+			return $is_subscription;
+		}
+
+		/**
 		 * @see WC_Memberships_Membership_Plans::grant_access_to_membership_from_order as example of memberships activation
 		 *
 		 * @param  WC_Order[]  $orders orders with one reepay subscription in each one
 		 */
 		public function maybe_activate_membership( $orders ) {
-			$membership_plans = $this->get_membership_plans();
-
+			$membership_plans = wc_memberships()->get_plans_instance()->get_membership_plans();
+			__log(
+				[
+					'source' => 'maybe_activate_membership !1!',
+					$membership_plans
+				]
+			);
 			if ( empty( $membership_plans ) ) {
 				return;
 			}
 
 			foreach ( $orders as $order ) {
-				$order_items      = $order->get_items();
-				$user_id          = $order->get_user_id();
+				$order = wc_get_order( $order );
+
+				__log(
+					[
+						'source' => 'maybe_activate_membership !2!',
+						$order
+					]
+				);
+
+				if ( empty( $order ) ) {
+					continue;
+				}
+
+				$order_items = $order->get_items();
+				$user_id     = $order->get_user_id();
+
+				__log(
+					[
+						'source' => 'maybe_activate_membership !3!',
+						$order_items,
+						$user_id
+					]
+				);
 
 				if ( empty( $order_items ) || empty( $user_id ) ) {
 					continue;
 				}
 
-				foreach ( $membership_plans as $plan) {
+				foreach ( $membership_plans as $plan ) {
+					__log(
+						[
+							'source' => 'maybe_activate_membership !4!',
+							$plan
+						]
+					);
 					if ( ! $plan->has_products() ) {
 						continue;
 					}
 
+					remove_filter( 'wc_memberships_access_granting_purchased_product_id', [ $this, 'disable_default_membership_activation_for_reepay_products' ], 100 );
 					$access_granting_product_ids = wc_memberships_get_order_access_granting_product_ids( $plan, $order, $order_items );
-
+					__log(
+						[
+							'source' => 'maybe_activate_membership !5!',
+							$access_granting_product_ids
+						]
+					);
 					foreach ( $access_granting_product_ids as $product_id ) { //TODO remove this loop
+						__log(
+							[
+								'source' => 'maybe_activate_membership !6!',
+								$product_id
+							]
+						);
 						if ( ! $plan->has_product( $product_id ) ) {
 							continue;
 						}
-
+						__log(
+							[
+								'source' => 'maybe_activate_membership !7!',
+								$product_id,
+								WC_Reepay_Renewals::is_order_subscription_active( $order )
+							]
+						);
 						if ( WC_Reepay_Renewals::is_order_subscription_active( $order ) ) {
-							$plan->grant_access_from_purchase( $user_id, $product_id, $order->get_id() );
+							$res = $plan->grant_access_from_purchase( $user_id, $product_id, $order->get_id() );
+							__log(
+								[
+									'source' => 'maybe_activate_membership !8!',
+									$res ? 'true' : 'false'
+								]
+							);
 						}
 					}
 				}
