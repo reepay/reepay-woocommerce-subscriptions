f8bcb2b5af5fc37807fe180fdb90002944b60cf8
daniel21703 <daniel217032001@gmail.com>
2023-06-27 00:23:55 +0300

    update subscriptions page

--- includes/my-account/WC_Reepay_My_Account_Subscriptions_Page.php
+++ includes/my-account/WC_Reepay_My_Account_Subscriptions_Page.php
@@ -41,28 +41,104 @@ class WC_Reepay_My_Account_Subscriptions_Page {
 	}
 
 	public function subscriptions_endpoint() {
-		$reepay_customer_handle = rp_get_customer_handle( get_current_user_id() );
+		try {
+			$reepay_customer_handle = rp_get_customer_handle( get_current_user_id() );
 
-		if ( empty( $reepay_customer_handle ) ) {
-			reepay()->get_template( 'myaccount/my-subscriptions-error.php', array(
-				'error' => esc_html__( 'You have no active subscriptions.', 'reepay-subscriptions-for-woocommerce' )
+			if ( empty( $reepay_customer_handle ) ) {
+				throw new Exception( esc_html__( 'You have no active subscriptions.', 'reepay-subscriptions-for-woocommerce' ) );
+			}
+
+			$reepay_subscriptions = reepay_s()->api()->request( "list/subscription?customer=$reepay_customer_handle&size=100" )['content'];
+
+			if ( empty( $reepay_subscriptions ) ) {
+				throw new Exception( esc_html__( 'You have no active subscriptions.', 'reepay-subscriptions-for-woocommerce' ) );
+			}
+
+			$plans = $this->get_plans_from_subscriptions( $reepay_subscriptions );
+
+			$subscriptions_data = array_map( function ( $reepay_subscription ) use ( $plans ) {
+				$subscription_data = [
+					'id'                => explode( '_', $reepay_subscription['handle'] )[0],
+					'link'              => wc_get_endpoint_url( WC_Reepay_My_Account_Subscription_Page::$menu_item_slug, $reepay_subscription['handle'], wc_get_page_permalink( 'myaccount' ) ),
+					'state'             => $reepay_subscription['state'],
+					'is_cancelled'      => $reepay_subscription['is_cancelled'],
+					'trial_end'         => $reepay_subscription['trial_end'] ?? null,
+					'next_period_start' => $reepay_subscription['next_period_start'] ?? null,
+					'plan' => $reepay_subscription['plan'],
+					'amount'            => '',
+					'billing_period'    => ''
+				];
+
+				$plan = $plans[ $reepay_subscription['plan'] ] ?? null;
+
+				if ( ! empty( $plan ) ) {
+					$subscription_data['amount']         = wc_price( rp_make_initial_amount( $plan['amount'], $plan['currency'] ) );
+					$subscription_data['billing_period'] = $this->get_billing_period_for_plan( $plan );
+				}
+
+				return $subscription_data;
+			}, $reepay_subscriptions );
+
+			reepay()->get_template( 'myaccount/my-subscriptions.php', array(
+				'subscriptions' => $subscriptions_data,
+				'plans'         => $plans
 			) );
+		} catch ( Exception $e ) {
+			reepay()->get_template( 'myaccount/my-subscriptions-error.php', array(
+				'error' => $e->getMessage()
+			) );
+		}
+	}
 
-			return;
+	/**
+	 * @param $subscriptions
+	 *
+	 * @return array
+	 */
+	private function get_plans_from_subscriptions( $subscriptions ) {
+		static $plans = [];
+
+		foreach ( $subscriptions as $subscription ) {
+			$plan_handle = $subscription['plan'];
+
+			if ( empty( $plans[ $plan_handle ] ) ) {
+				try {
+					$plans[ $plan_handle ] = reepay_s()->api()->request( "plan/{$plan_handle}/current" );
+				} catch (Exception $e) {
+					continue;
+				}
+			}
 		}
 
-		$subscriptions = reepay_s()->api()->request( "list/subscription?customer=$reepay_customer_handle" );
+		return $plans;
+	}
 
-		if ( empty( $subscriptions ) ) {
-			reepay()->get_template( 'myaccount/my-subscriptions-error.php', array(
-				'error' => esc_html__( 'You have no active subscriptions.', 'reepay-subscriptions-for-woocommerce' )
-			) );
+	/**
+	 * @param array $plan_data Reepay plan data
+	 *
+	 * @return string
+	 */
+	private function get_billing_period_for_plan( $plan_data ) {
+		static $billing_periods = [];
 
-			return;
+		if ( isset( $billing_periods[ $plan_data['handle'] ] ) ) {
+			return $billing_periods[ $plan_data['handle'] ];
 		}
 
-		reepay()->get_template( 'myaccount/my-subscriptions.php', array(
-			'subscriptions' => $subscriptions
-		) );
+		$plan_meta_data = reepay_s()->plan()->get_remote_plan_meta( $plan_data );
+
+		$schedule_type = $plan_meta_data['_reepay_subscription_schedule_type'];
+		$schedule_data = $plan_meta_data[ $schedule_type ] ?? $plan_data['interval_length'] ?? array();
+
+		$billing_periods[ $plan_data['handle'] ] = WC_Reepay_Subscription_Plan_Simple::get_billing_plan(
+			array(
+				'type'      => $schedule_type,
+				'type_data' => $schedule_data,
+				'interval'  => $plan_data['interval_length']
+			),
+			true
+		);
+
+		return $billing_periods[ $plan_data['handle'] ];
 	}
 }
--- templates/woocommerce/myaccount/my-subscriptions.php
+++ templates/woocommerce/myaccount/my-subscriptions.php
@@ -6,7 +6,9 @@
  * @category WooCommerce Subscriptions/Templates
  * @version  2.6.4
  *
- * @var array $subscriptions Reepay subscriptions https://reference.reepay.com/api/#the-subscription-object
+ * @var array $subscriptions subscriptions data to output
+ *
+ * @see WC_Reepay_My_Account_Subscriptions_Page::subscriptions_endpoint
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -14,70 +16,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div class="woocommerce_account_subscriptions">
+    <table class="my_account_subscriptions my_account_orders woocommerce-orders-table woocommerce-MyAccount-subscriptions shop_table shop_table_responsive woocommerce-orders-table--subscriptions">
 
-	<?php if ( ! empty( $subscriptions ) ) : ?>
-        <table class="my_account_subscriptions my_account_orders woocommerce-orders-table woocommerce-MyAccount-subscriptions shop_table shop_table_responsive woocommerce-orders-table--subscriptions">
+        <thead>
+        <tr>
+            <th class="subscription-id order-number woocommerce-orders-table__header woocommerce-orders-table__header-order-number woocommerce-orders-table__header-subscription-id">
+                <span class="nobr"><?php esc_html_e( 'Subscription', 'reepay-subscriptions-for-woocommerce' ); ?></span></th>
+            <th class="subscription-status order-status woocommerce-orders-table__header woocommerce-orders-table__header-order-status woocommerce-orders-table__header-subscription-status">
+                <span class="nobr"><?php esc_html_e( 'Status', 'reepay-subscriptions-for-woocommerce' ); ?></span></th>
+            <th class="subscription-next-payment order-date woocommerce-orders-table__header woocommerce-orders-table__header-order-date woocommerce-orders-table__header-subscription-next-payment">
+                <span class="nobr"><?php echo esc_html_x( 'Next payment', 'table heading', 'reepay-subscriptions-for-woocommerce' ); ?></span>
+            </th>
+            <th class="subscription-total order-total woocommerce-orders-table__header woocommerce-orders-table__header-order-total woocommerce-orders-table__header-subscription-total">
+                <span class="nobr"><?php echo esc_html_x( 'Amount', 'table heading', 'reepay-subscriptions-for-woocommerce' ); ?></span>
+            </th>
+            <th class="subscription-actions order-actions woocommerce-orders-table__header woocommerce-orders-table__header-order-actions woocommerce-orders-table__header-subscription-actions">
+                &nbsp;
+            </th>
+        </tr>
+        </thead>
 
-            <thead>
-            <tr>
-                <th class="subscription-id order-number woocommerce-orders-table__header woocommerce-orders-table__header-order-number woocommerce-orders-table__header-subscription-id">
-                    <span class="nobr"><?php esc_html_e( 'Subscription', 'reepay-subscriptions-for-woocommerce' ); ?></span></th>
-                <th class="subscription-status order-status woocommerce-orders-table__header woocommerce-orders-table__header-order-status woocommerce-orders-table__header-subscription-status">
-                    <span class="nobr"><?php esc_html_e( 'Status', 'reepay-subscriptions-for-woocommerce' ); ?></span></th>
-                <th class="subscription-next-payment order-date woocommerce-orders-table__header woocommerce-orders-table__header-order-date woocommerce-orders-table__header-subscription-next-payment">
-                    <span class="nobr"><?php echo esc_html_x( 'Next payment', 'table heading', 'reepay-subscriptions-for-woocommerce' ); ?></span>
-                </th>
-                <th class="subscription-total order-total woocommerce-orders-table__header woocommerce-orders-table__header-order-total woocommerce-orders-table__header-subscription-total">
-                    <span class="nobr"><?php echo esc_html_x( 'Total', 'table heading', 'reepay-subscriptions-for-woocommerce' ); ?></span>
-                </th>
-                <th class="subscription-actions order-actions woocommerce-orders-table__header woocommerce-orders-table__header-order-actions woocommerce-orders-table__header-subscription-actions">
-                    &nbsp;
-                </th>
+        <tbody>
+		<?php foreach ( $subscriptions as $subscription ) : ?>
+            <tr class="order woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $subscription['state'] ); ?>">
+                <td class="subscription-id order-number woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-id woocommerce-orders-table__cell-order-number"
+                    data-title="<?php esc_attr_e( 'ID', 'reepay-subscriptions-for-woocommerce' ); ?>">
+                    <a href="<?php echo esc_url( $subscription['link'] ); ?>"><?php echo esc_html( sprintf( _x( '#%s', 'hash before order number', 'reepay-subscriptions-for-woocommerce' ), $subscription['id'] ) ); ?></a>
+                </td>
+                <td class="subscription-status order-status woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-status woocommerce-orders-table__cell-order-status"
+                    data-title="<?php esc_attr_e( 'Status', 'reepay-subscriptions-for-woocommerce' ); ?>">
+	                <?php echo esc_html( WC_Reepay_My_Account_Subscription_Page::get_status( $subscription ) ); ?>
+                </td>
+                <td class="subscription-next-payment order-date woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-next-payment woocommerce-orders-table__cell-order-date"
+                    data-title="<?php echo esc_attr_x( 'Next Payment', 'table heading', 'reepay-subscriptions-for-woocommerce' ); ?>">
+	                <?php echo ! empty( $subscription['next_period_start'] ) ? wp_date( get_option( 'date_format' ), strtotime( $subscription['next_period_start'] ) ) : '' ?>
+                </td>
+                <td class="subscription-total order-total woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-total woocommerce-orders-table__cell-order-total"
+                    data-title="<?php echo esc_attr_x( 'Total', 'Used in data attribute. Escaped', 'reepay-subscriptions-for-woocommerce' ); ?>">
+	                <?php echo $subscription['amount'] . ' / ' . $subscription['billing_period'] ?>
+                </td>
+                <td class="subscription-actions order-actions woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-actions woocommerce-orders-table__cell-order-actions">
+                    <a href="<?php echo esc_url( $subscription['link'] ) ?>"
+                       class="woocommerce-button button view"><?php echo esc_html_x( 'View', 'view a subscription', 'reepay-subscriptions-for-woocommerce' ); ?></a>
+                </td>
             </tr>
-            </thead>
+		<?php endforeach; ?>
+        </tbody>
 
-            <tbody>
-			<?php /** @var WC_Subscription|array $subscription */ ?>
-			<?php foreach ( $subscriptions as $subscription_handle => $subscription_data ) :
-
-				?>
-                <tr class="order woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $subscription->get_status() ); ?>">
-                    <td class="subscription-id order-number woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-id woocommerce-orders-table__cell-order-number"
-                        data-title="<?php esc_attr_e( 'ID', 'reepay-subscriptions-for-woocommerce' ); ?>">
-                        <a href="<?php echo esc_url( $link ); ?>"><?php echo esc_html( sprintf( _x( '#%s', 'hash before order number', 'reepay-subscriptions-for-woocommerce' ), $subscription->get_order_number() ) ); ?></a>
-						<?php do_action( 'woocommerce_my_subscriptions_after_subscription_id', $subscription ); ?>
-                    </td>
-                    <td class="subscription-status order-status woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-status woocommerce-orders-table__cell-order-status"
-                        data-title="<?php esc_attr_e( 'Status', 'reepay-subscriptions-for-woocommerce' ); ?>">
-						<?php echo esc_html( wc_get_order_status_name( $subscription->get_status() ) ); ?>
-                    </td>
-                    <td class="subscription-next-payment order-date woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-next-payment woocommerce-orders-table__cell-order-date"
-                        data-title="<?php echo esc_attr_x( 'Next Payment', 'table heading', 'reepay-subscriptions-for-woocommerce' ); ?>">
-						<?php echo WC_Reepay_Renewals::get_reepay_subscription_dates( $subscription, 'next_period_start' ) ?: '-' ?>
-                    </td>
-                    <td class="subscription-total order-total woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-total woocommerce-orders-table__cell-order-total"
-                        data-title="<?php echo esc_attr_x( 'Total', 'Used in data attribute. Escaped', 'reepay-subscriptions-for-woocommerce' ); ?>">
-						<?php echo $subscription->get_formatted_order_total() ?><?php echo ! empty( $billing_type ) ? ' / ' . $billing_type : '' ?>
-                    </td>
-                    <td class="subscription-actions order-actions woocommerce-orders-table__cell woocommerce-orders-table__cell-subscription-actions woocommerce-orders-table__cell-order-actions">
-                        <a href="<?php echo esc_url( $link ) ?>"
-                           class="woocommerce-button button view"><?php echo esc_html_x( 'View', 'view a subscription', 'reepay-subscriptions-for-woocommerce' ); ?></a>
-						<?php do_action( 'woocommerce_my_subscriptions_actions', $subscription ); ?>
-                    </td>
-                </tr>
-			<?php endforeach; ?>
-            </tbody>
-
-        </table>
-	<?php else : ?>
-        <p class="no_subscriptions woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
-            <?php esc_html_e( 'You have no active subscriptions.', 'reepay-subscriptions-for-woocommerce' ); ?>
-
-            <a class="woocommerce-Button button"
-               href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-		        <?php esc_html_e( 'Browse products', 'reepay-subscriptions-for-woocommerce' ); ?>
-            </a>
-        </p>
-	<?php endif; ?>
-
+    </table>
 </div>
