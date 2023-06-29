223e951bf55988d8d649620d189e3beafd5034db
daniel21703 <daniel217032001@gmail.com>
2023-06-29 17:07:43 +0300

    update add-payment-method page

--- includes/my-account/WC_Reepay_My_Account.php
+++ includes/my-account/WC_Reepay_My_Account.php
@@ -2,7 +2,6 @@
 
 class WC_Reepay_My_Account {
 	public function __construct() {
-		new WC_Reepay_My_Account_Add_Payment_Method_Page();
 		new WC_Reepay_My_Account_Orders_Page();
 		new WC_Reepay_My_Account_Payment_Method();
 		new WC_Reepay_My_Account_Subscription_Page();
