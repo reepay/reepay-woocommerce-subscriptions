83c5b18a47bc9597b22f927a094f95a12467f057
dimaspolohov@yandex.ru <Zenitkms123>
2023-07-06 15:00:19 +0300

    Set order type for my account

--- includes/my-account/WC_Reepay_My_Account_Orders_Page.php
+++ includes/my-account/WC_Reepay_My_Account_Orders_Page.php
@@ -3,12 +3,14 @@
 class WC_Reepay_My_Account_Orders_Page {
 	public function __construct() {
 		add_filter( 'woocommerce_account_orders_columns', [ $this, 'add_column_to_account_orders' ], 2, 10 );
-		add_filter( 'woocommerce_my_account_my_orders_column_order_type', [ $this, 'add_order_type_to_account_orders' ], 2, 10 );
-		add_filter( 'woocommerce_get_formatted_order_total', [ $this, 'show_zero_order_total_on_account_orders' ], 1, 10 );
+		add_filter( 'woocommerce_my_account_my_orders_column_order_type', [ $this, 'add_order_type_to_account_orders' ],
+			2, 10 );
+		add_filter( 'woocommerce_get_formatted_order_total', [ $this, 'show_zero_order_total_on_account_orders' ], 1,
+			10 );
 	}
 
 	/**
-	 * @param array $columns
+	 * @param  array  $columns
 	 */
 	public function add_column_to_account_orders( $columns ) {
 		$columns['order_type'] = __( 'Order type', 'reepay-subscriptions-for-woocommerce' );
@@ -17,7 +19,7 @@ class WC_Reepay_My_Account_Orders_Page {
 	}
 
 	/**
-	 * @param WC_Order $order
+	 * @param  WC_Order  $order
 	 */
 	public function add_order_type_to_account_orders( $order ) {
 		$type = '';
@@ -29,9 +31,10 @@ class WC_Reepay_My_Account_Orders_Page {
 
 			if ( ! empty( $order_items ) ) {
 				$product = current( $order_items )->get_product();
-
 				if ( WC_Subscriptions_Product::is_subscription( $product ) ) {
 					$type = __( 'Subscription', 'reepay-subscriptions-for-woocommerce' );
+				} elseif ( ! empty( $order->get_meta( '_reepay_order' ) ) && $order->get_parent_id() != 0 ) {
+					$type = __( 'Renewal', 'reepay-subscriptions-for-woocommerce' );
 				} else {
 					$type = __( 'Order', 'reepay-subscriptions-for-woocommerce' );
 				}
